c582c298aef9f1c80a9c3a8a3a6d769301f9171b
Ekaterina Davliatchina <flykat@yandex.ru>
2017-04-02 02:51:18 +1000

    Промежуточная проверка

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 //mobile navigation
+
 var navPage = document.querySelector('.page-header__nav');
     var navBtn = document.querySelector('.page-header__btn');
 
@@ -13,3 +14,67 @@ var navPage = document.querySelector('.page-header__nav');
         navPage.classList.remove('page-header__nav--opened');
       }
     });
+
+
+//  interactive map
+
+ymaps.ready(init);
+
+function init() {
+	var myMap = new ymaps.Map('map', {
+		center: [59.93923400570833, 30.329127021209725],
+		zoom: 16,
+		controls: []
+	});
+
+	myMap.behaviors.disable('scrollZoom');
+
+	myMap.controls.add("zoomControl", {
+		position: {
+			top: 15,
+			left: 15
+		}
+	});
+
+	var myPlacemark = new ymaps.Placemark(
+		[59.93863106417265, 30.3230545], {}, {
+			iconLayout: 'default#image',
+			iconImageHref: 'img/icon-map-pin.svg',
+			iconImageSize: [66, 100],
+			iconImageOffset: [-66, -100] //todo
+		});
+
+	myMap.geoObjects.add(myPlacemark);
+
+}
+
+// modal
+
+var cart = document.querySelector(".product__cart");
+var order = document.querySelector(".popular-product__btn");
+var popup = document.querySelector(".modal");
+var close = document.querySelector(".form-select__btn");
+
+cart.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.add("modal-show");
+});
+
+order.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.remove("modal-show");
+});
+
+
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode == 27) {
+		if (popup.classList.contains("modal-show")) {
+			popup.classList.remove("modal-show");
+		}
+	}
+});
